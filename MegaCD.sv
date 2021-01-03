e56a322a47a24d33b463e784c4a7f0ea3a270368
Shane Lynch <794689+iequalshane@users.noreply.github.com>
2021-01-03 06:16:01 -0800

    Adding auto-region toggle for cartridge games. (#58)
    * Adding auto-region toggle for cartridge games.
    Allows toggling region-specific content by disabling auto-region detection in Pier Solar/MSU-MD games and helps bypass bad header region designations.
    
    * Reset MegaCD hardware when cartridge is loading to prevent games from running before the cartridge is ready.

--- MegaCD.sv
+++ MegaCD.sv
@@ -189,6 +189,7 @@ localparam CONF_STR = {
 	"S0,CUE,Insert Disk;",
 	"-;",
 	"O67,Region,JP,US,EU;",
+	"oH,Auto Cart Region,Disabled,Header;",
 	"-;",
 	"C,Cheats;",
 	"H5OO,Cheats Enabled,Yes,No;",
@@ -586,7 +587,7 @@ wire        gg_available2;
 
 MCD MCD
 (
-	.RST_N(~reset),
+	.RST_N(~(reset|rom_download)),
 	.CLK(clk_sys),
 	.ENABLE(1),
 	.MCD_RST_N(MCD_RST_N),
@@ -1055,7 +1056,7 @@ always @(posedge clk_sys) begin
 	end
 
 	old_ready <= cart_hdr_ready;
-	if(~old_ready & cart_hdr_ready) begin
+	if(status[49] & ~old_ready & cart_hdr_ready) begin
 		region_set <= 1;
 		if(hdr_u) region_req <= 1;
 		else if(hdr_j) region_req <= 0;
